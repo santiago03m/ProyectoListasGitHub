7f182c5287990146ef58b93aeec5b57907547226
Santiago Rueda <santiruedadhhd@gmail.com>
2024-05-01 09:17:10 -0500

    se modifican las clases existentes para funcionar con la interfaz

--- Libreta.py
+++ Libreta.py
@@ -2,5 +2,5 @@ class Libreta:
     def __init__(self):
         self.libreta = []
     
-    def guardar_contacto(self, contacto):
+    def guardar_contacto(self, contacto=None):
         self.libreta.append(contacto)
