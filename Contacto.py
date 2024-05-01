abe1b57061535cc280aa2f06daead5210cfa6607
Santiago Rueda <santiruedadhhd@gmail.com>
2024-05-01 08:30:21 -0500

    se crea la clase libreta y se mueve el metodo agregar_contacto

--- Contacto.py
+++ Contacto.py
@@ -1,8 +1,6 @@
+
 class Contacto:
     def __init__(self, nombre, numero):
         self.numero = numero
         self.nombre = nombre
-    
-    def guardar_contacto(contacto):
-        contactos = []
-        contactos.append(contacto)
+        self.libreta = []
