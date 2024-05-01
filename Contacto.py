477ab8cb59937febb75c9d01d781773ffd59357e
Santiago Rueda <santiruedadhhd@gmail.com>
2024-04-30 23:48:16 -0500

    añado la clase contacto

---
+++ Contacto.py
@@ -0,0 +1,8 @@
+class Contacto:
+    def __init__(self, nombre, numero):
+        self.numero = numero
+        self.nombre = nombre
+    
+    def guardar_contacto(contacto):
+        contactos = []
+        contactos.append(contacto)
